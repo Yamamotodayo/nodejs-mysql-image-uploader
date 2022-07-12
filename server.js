1cc9e6aa399f1b11bce81e94486e7e30ec09ea3f
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-13 01:48:51 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { engine } = require('express-handlebars');
+const app = express();
+
+const PORT = 5000;
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(PORT, () => console.log("サーバー起動中..."));
